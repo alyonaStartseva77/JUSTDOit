e5f0ada7ad7e1ec9abefbac00a57ab24f7ce2222
Alyona Startseva <alyona_startseva.tk@mail.ru>
2021-04-28 22:54:51 +0700

    Connected libraries and task-manager Connected Bootstrap and added Gulp for compilation from SASS to CSS and minify CSS/HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+'use strict'
 
-exports.default = defaultTask
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const csso = require('gulp-csso')
+const htmlmin = require('gulp-htmlmin');
+
+sass.compiler = require('node-sass')
+
+//compilation from SASS to CSS and minify CSS
+gulp.task('styles', function () {
+    return gulp.src('src/sass/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(csso())
+        .pipe(gulp.dest('src/css'))
+})
+
+gulp.task('minifyHTML', () => {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
