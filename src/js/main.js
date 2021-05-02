71501f23b2e1dcbbe49c85a46e055a35c089113e
Alyona Startseva <alyona_startseva.tk@mail.ru>
2021-05-02 16:21:18 +0700

    Added layout for to-do lists Left part of the layout is completely done: added display of to-do lists and creation

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// const list = document.querySelector('.todo__item')
+// const btnClose = document.querySelector('.close')
+//
+//
+// list.addEventListener('click', () => {
+//     console.log('Click on list')
+// })
+//
+// btnClose.addEventListener('click', () => {
+//     console.log('Click on close')
+// })
